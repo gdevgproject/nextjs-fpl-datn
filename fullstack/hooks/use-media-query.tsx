5cdecc3022603ea1ac5f33ee24546bf67dd81fad
VuDucMinh <gdev.gproject@gmail.com>
2025-03-28 01:24:55 +0700

    feat: media query hook

---
+++ fullstack/hooks/use-media-query.tsx
@@ -0,0 +1,30 @@
+"use client"
+
+import { useEffect, useState } from "react"
+
+export function useMediaQuery(query: string): boolean {
+  const [matches, setMatches] = useState(false)
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia(query)
+
+    // Set initial value
+    setMatches(mediaQuery.matches)
+
+    // Define callback function to handle changes
+    const handleChange = (event: MediaQueryListEvent) => {
+      setMatches(event.matches)
+    }
+
+    // Add event listener
+    mediaQuery.addEventListener("change", handleChange)
+
+    // Clean up
+    return () => {
+      mediaQuery.removeEventListener("change", handleChange)
+    }
+  }, [query])
+
+  return matches
+}
+
